63915c47ebc8e719d5e65df0515058edf3cdb8c0
AlexeyTolmachov <taar12sh@gmail.com>
2023-10-05 15:44:11 +0300

    ads img   connect dynamic component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import DynamicRenderingReviews from './components/DynamicRenderingReviews/DynamicRenderingReviews';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import StaticSection from './components/StaticSection/StaticSection';
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header />
       <StaticSection />
+      <DynamicRenderingReviews />
       <Footer />
     </div>
   );
