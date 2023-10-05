4ac90a4d3467558fc2662aaf5ab73f7e53142a65
AlexeyTolmachov <taar12sh@gmail.com>
2023-10-05 12:26:59 +0300

    fised app,css add components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import StaticSection from './components/StaticSection/StaticSection';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <StaticSection />
+      <Footer />
     </div>
   );
 }
