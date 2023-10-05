4676485e27dadf72ef3a7fc27b62803fb9f8471b
AlexeyTolmachov <taar12sh@gmail.com>
2023-10-05 23:40:32 +0300

    fixed pagination

--- src/components/DynamicRenderingReviews/DynamicRenderingReviews.jsx
+++ src/components/DynamicRenderingReviews/DynamicRenderingReviews.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
-import "./DynamicRenderingReviews.css"; // Импортируйте файл стилей
+import "./DynamicRenderingReviews.css";
 
 const DynamicRenderingReviews = () => {
   const [reviews, setReviews] = useState([]);
   const [page, setPage] = useState(1);
-  const reviewsPerPage = 15; // Количество отзывов на одной странице
+  const reviewsPerPage = 15;
 
   useEffect(() => {
     const fetchReviews = async () => {
@@ -14,19 +14,56 @@ const DynamicRenderingReviews = () => {
           const data = await response.json();
           setReviews(data);
         } else {
-          console.error("Ошибка получения данных");
+          console.error("Error receiving data");
         }
       } catch (error) {
-        console.error("Произошла ошибка:", error);
+        console.error("An error has occurred:", error);
       }
     };
 
     fetchReviews();
   }, []);
 
+  const totalPages = Math.ceil(reviews.length / reviewsPerPage);
+
+  const handlePageClick = (newPage) => {
+    if (newPage >= 1 && newPage <= totalPages) {
+      setPage(newPage);
+    }
+  };
+
   const startIndex = (page - 1) * reviewsPerPage;
   const endIndex = startIndex + reviewsPerPage;
   const displayedReviews = reviews.slice(startIndex, endIndex);
+
+  const pageButtons = [];
+  const visiblePageRange = 3;
+
+  for (let i = 1; i <= totalPages; i++) {
+    if (
+      i === 1 ||
+      i === totalPages ||
+      (i >= page - 1 && i <= page + 1) ||
+      (i <= visiblePageRange && page <= visiblePageRange + 1) ||
+      (i >= totalPages - visiblePageRange + 1 &&
+        page >= totalPages - visiblePageRange)
+    ) {
+      pageButtons.push(
+        <button
+          key={i}
+          onClick={() => handlePageClick(i)}
+          className={i === page ? "page-button active-page" : "page-button"}
+        >
+          {i}
+        </button>
+      );
+    } else if (
+      (i === 2 && page > visiblePageRange + 2) ||
+      (i === totalPages - 1 && page < totalPages - visiblePageRange)
+    ) {
+      pageButtons.push(<span key="ellipsis">...</span>);
+    }
+  }
 
   return (
     <div className="reviews-container">
@@ -35,7 +72,7 @@ const DynamicRenderingReviews = () => {
         {displayedReviews.map((review) => (
           <div className="review-card" key={review.id}>
             <div className="infoOfReview">
-              <p> {review.employee}</p>
+              <p>{review.employee}</p>
               <p>
                 {review.employee_position}- {review.company}
               </p>
@@ -44,7 +81,7 @@ const DynamicRenderingReviews = () => {
             <div className="user-info">
               <div className="user-photo">
                 <div>
-                  <img src="/Avatar (1).svg" alt="Логотип" />
+                  <img src="/Avatar (1).svg" alt="Avatar" />
                 </div>
               </div>
               <div className="user-details">
@@ -68,15 +105,22 @@ const DynamicRenderingReviews = () => {
       </div>
 
       <div className="pagination">
-        <button onClick={() => setPage(page - 1)} disabled={page === 1}>
-          Предыдущая страница
-        </button>
-        <span>Страница {page}</span>
         <button
-          onClick={() => setPage(page + 1)}
-          disabled={endIndex >= reviews.length}
+          onClick={() => handlePageClick(page - 1)}
+          className={page === 1 ? "page-button disabled" : "page-button"}
+          disabled={page === 1}
         >
-          Следующая страница
+          &lt;
+        </button>
+        {pageButtons}
+        <button
+          onClick={() => handlePageClick(page + 1)}
+          className={
+            page === totalPages ? "page-button disabled" : "page-button"
+          }
+          disabled={page === totalPages}
+        >
+          &gt;
         </button>
       </div>
     </div>
