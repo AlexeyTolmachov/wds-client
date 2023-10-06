9a08061d846f81b66ba4c456a68084ae28adbc7b
AlexeyTolmachov <taar12sh@gmail.com>
2023-10-06 21:51:30 +0300

    fixed double quote

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import './App.css';
 import HomePage from './components/HomePage';
 import { Route, Routes } from 'react-router';
 import AdminPanel from './components/AdminPanel/AdminPanel';
+import AuthForm from './components/AuthForm/AuthForm';
 
 
 
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
 
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='admin' element={<AdminPanel />} />
+        <Route path='form' element={<AuthForm />} />
       </Routes>
     </div>
   );
